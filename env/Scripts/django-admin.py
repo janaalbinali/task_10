51910ab0c346400fe3a4019a4353102aaaf04201
Jana Bin Ail <jwalbinali@gmail.com>
2020-01-30 13:51:13 +0300

    tasktendone

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jana bin ail\desktop\development\taskten\task_10\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
